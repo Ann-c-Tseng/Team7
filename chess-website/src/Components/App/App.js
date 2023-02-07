9861c0b68c3038ab2501cda97f25ace80a9b3d41
Parker Gutierrez <mrnibblesgutierrez@gmail.com>
2023-02-06 19:32:35 -0800

    Chess game now only accepts valid moves

--- chess-website/src/Components/App/App.js
+++ chess-website/src/Components/App/App.js
@@ -44,19 +44,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// return (
-//   <>
-//   <div className="App">
-//     <header className="App-header">
-//     </header>
-    
-//   </div>
-//   <Chessboard 
-//       className="Chessboard"
-//       boardWidth={800}
-//       boardHeight={800}
-//     />
-//   </>
-// );
---
+++ chess-website/src/Components/ChessGame/ChessGame.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+
+class ChessGame extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {game: undefined};
+        this.onDrop = this.onDrop.bind(this);
+    }
+    componentDidMount(){
+        this.setState({game: new Chess()});
+    }
+
+    makeMove(move){
+        const moveResult = this.state.game.move(move);
+        this.setState({game: this.state.game});
+        return moveResult;
+    }
+
+    onDrop(fromSquare, toSquare){
+        const move = this.makeMove({
+            from: fromSquare,
+            to: toSquare,
+            promotion: "q"
+        });
+        return move ? true : false;
+    }
+
+    render(){
+        return (
+            this.state.game ?
+            <Chessboard 
+                className="Chessboard"
+                boardWidth={800}
+                boardHeight={800}
+                position={this.state.game.fen()}
+                onPieceDrop={this.onDrop}
+            /> : null
+        );
+    }
+}
+
+export default ChessGame;
