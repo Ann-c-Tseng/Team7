04faef0c4e621e298c1332c429af3d60061680eb
Ann Tseng <anntseng@Anns-MacBook-Pro.local>
2023-02-20 21:44:12 -0800

    signup now connected to mongoDB

--- chess-website/client/src/Components/Forms/SignupForms.js
+++ chess-website/client/src/Components/Forms/SignupForms.js
@@ -1,29 +1,113 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Forms.css';
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
 
-function SignupForm() {
-    return (
-        <div className="card">
-            <form>
-                <div>
-                    <h2 className="title"> Sign up</h2>
+class SignupForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            fullName:'',
+            username:'',
+            email:'',
+            password:''
+        }
+        this.changeFullName = this.changeFullName.bind(this)
+        this.changeUsername = this.changeUsername.bind(this)
+        this.changeEmail = this.changeEmail.bind(this)
+        this.changePassword = this.changePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    changeFullName(event){
+        this.setState({
+            fullName:event.target.value
+        })
+    }
+
+    changeUsername(event){
+        this.setState({
+            username:event.target.value
+        })
+    }
+
+    changeEmail(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    changePassword(event){
+        this.setState({
+            password:event.target.value
+        })
+    }
+
+    onSubmit(event){
+        event.preventDefault()
+        const registered = {
+            fullName: this.state.fullName,
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        //Super Important: Connects to Server and MongoDB
+        axios.post('http://localhost:4000/signup', registered)
+        .then(response => console.log(response.data))
+
+        // window.location = '/' //In a normal application, we may send user back home after sign up...
+    
+        //For now though, we just return the state back to empty
+        this.setState({
+            fullName:'',
+            username:'',
+            email:'',
+            password:''
+        })
+    }
+
+
+    render() { 
+        return (
+            <div>
+                <div className='container'>
+                    <div className='form-div'>
+                        <form onSubmit={this.onSubmit}>
+                            <input type = 'text'
+                            placeholder="Full Name"
+                            onChange={this.changeFullName}
+                            value={this.state.fullName}
+                            className='form-control form-group'
+                            />
+
+                            <input type='text'
+                            placeholder='Username'
+                            onChange={this.changeUsername}
+                            value={this.state.username}
+                            className='form-control form-group'
+                            />
+
+                            <input type='text'
+                            placeholder='E-mail'
+                            onChange={this.changeEmail}
+                            value={this.state.email}
+                            className='form-control form-group'
+                            />
+
+                            <input type='password'
+                            placeholder='Password'
+                            onChange={this.changePassword}
+                            value={this.state.password}
+                            className='form-control form-group'
+                            />
+
+                            <input type='submit' className='btn btn-danger btn-block' value='Submit'/>
+                        </form>
+                    </div>
                 </div>
-
-                <div className="email-login">
-                    <label htmlFor="email"> <b>Email</b></label>
-                    <input type="text" placeholder="name@abc.com" name="uname" required />
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" required />
-                    <label htmlFor="psw"><b>Confirm Password</b></label>
-                    <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" required />
-                </div>
-                <button className="cta-btn">Sign up</button>
-
-                <p className="subtitle">Already have an account? <Link to="/login"> Sign in</Link></p>
-            </form>
-        </div>
-    );
+            </div>
+        );
+    }
 }
-
+ 
 export default SignupForm;
