d98b3daf9efee9882749786138ad575106476f65
Parker Gutierrez <mrnibblesgutierrez@gmail.com>
2023-03-05 22:02:49 -0800

    Other card says opponent by default

--- chess-website/client/src/Components/ChessPage/ChessPage.js
+++ chess-website/client/src/Components/ChessPage/ChessPage.js
@@ -379,7 +379,7 @@ class ChessPage extends React.Component{
                         />
                         :
                         <UserCard className="UserCard"
-                            username="User"
+                            username="Opponent"
                             elo={1000}
                         />
                     }
