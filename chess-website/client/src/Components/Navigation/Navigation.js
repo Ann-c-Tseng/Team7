b873095a8568911975b04763030799fa072a27e4
saunder1 <vanwoes2@wwu.edu>
2023-03-08 10:29:09 -0800

    new navbar

--- chess-website/client/src/Components/Navigation/Navigation.js
+++ chess-website/client/src/Components/Navigation/Navigation.js
@@ -1,40 +1,94 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 import "./Navigation.css";
 
-class Navigation extends React.Component {
-  render() {
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+
+const Navigation = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const defaultButtons = [
+    <Link to="/" className="link"><button className="nav-home">Chess Master</button></Link>,
+    <Link to="/login" className="link"><button className="nav-button">Log In</button></Link>,
+    <Link to="/signup" className="link"><button className="nav-button">Sign Up</button></Link>
+  ]
+
+  const authenticatedButtons = [
+    <Link to="/" className="link"><button className="nav-home">Chess Master</button></Link>,
+    <Link to="/chess" className="link"><button className="nav-button">Play Chess!</button></Link>,
+    <Link to="/profile" className="link"><button className="nav-button">Profile</button></Link>,
+    <Link to="/history" className="link"><button className="nav-button">History</button></Link>,
+    <Link to="/leaderboard" className="link"><button className="nav-button">Leaderboard</button></Link>,
+    <Link to="/rules" className="link"><button className="nav-button">Rules</button></Link>
+  ]
+
+  if (isAuthenticated) {
     return (
-      <div>
-        <nav>
-          <ul>
-            {this.props.isAuthenticated ? 
-            <>
-              <li><Link to="/chess"><button>Play Chess!</button></Link></li>
-              <li><Link to="/profile"><button>Profile</button></Link></li>
-              <li><Link to="/history"><button>History</button></Link></li>
-              <li><Link to="/leaderboard"><button>Leaderboard</button></Link></li>
-              <li><Link to="/rules"><button>Rules</button></Link></li>
-            </>
-            :
-            <>
-              <li><Link to="/login"><button>Log In</button></Link></li>
-              <li><Link to="/signup"><button>Sign Up</button></Link></li>
-            </>
-            }
-          </ul>
-        </nav>
-      </div>
-    );
+      <Box className="nav">
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical outlined button group"
+          variant="text"
+        >
+          {authenticatedButtons}
+        </ButtonGroup>
+      </Box>
+    )
+  } else {
+    return (
+      <Box className="nav">
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical outlined button group"
+          variant="text"
+        >
+          {defaultButtons}
+        </ButtonGroup>
+      </Box>
+    )
   }
+  
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.auth.user,
-    isAuthenticated: state.auth.isAuthenticated
-  }
-}
+export default Navigation;
 
-export default connect(mapStateToProps)(Navigation);
+
+
+
+
+
+
+
+
+
+
+// class Navigation extends React.Component {
+//   render() {
+//     return (
+//       <div className="nav-container">
+//         <nav>
+//           <ul>
+//             <li><Link to="/"><button className='home'>Chess Master</button></Link></li>
+//             {this.props.isAuthenticated ? 
+//             <>
+//               <li><Link to="/chess"><button>Play Chess!</button></Link></li>
+//               <li><Link to="/profile"><button>Profile</button></Link></li>
+//               <li><Link to="/history"><button>History</button></Link></li>
+//               <li><Link to="/leaderboard"><button>Leaderboard</button></Link></li>
+//               <li><Link to="/rules"><button>Rules</button></Link></li>
+//             </>
+//             :
+//             <>
+//               <li><Link to="/login"><button>Log In</button></Link></li>
+//               <li><Link to="/signup"><button>Sign Up</button></Link></li>
+//             </>
+//             }
+//           </ul>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
