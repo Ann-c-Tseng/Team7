1f99afc84380f802e88866fed7a00f1d961592b3
Parker Gutierrez <mrnibblesgutierrez@gmail.com>
2023-03-09 17:23:30 -0800

    Profile page improved

--- chess-website/client/src/Components/Pages/Profile.js
+++ chess-website/client/src/Components/Pages/Profile.js
@@ -2,47 +2,35 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import {useSelector, useDispatch} from "react-redux";
+import {Box, Button, Typography} from "@mui/material";
+
+import "./Profile.css";
+
+const textColor = "#fefefedf"
+const bodyTypographyStyling = {
+    color: textColor,
+    textOverflow: "break-word"
+}
 
 const Profile = () => {
     const [isLoading, setLoading] = useState(true);
     const [userData, setUserData] = useState();
 
-    // const userProfilePic = require('../../Images/tentativeProfile.png');
-    const userProfile = useSelector((state) => state.auth.user.avatar);
     const username = useSelector((state) => state.auth.user.username);
     const useremail = useSelector((state) => state.auth.user.email);
     const dispatch = useDispatch();
 
-    const overallDivStyle = {
-        width: '40vw',
-        padding: '3em',
-        backgroundColor: 'white',
-        borderRadius: 10,
-    }
-
-    const ProfilePicStyle = {
-        flex: 1,
-        width: 200, 
-        height: 200, 
-        borderRadius: 200/2,
-        resizeMode: 'contain',
-    };
-
-    const h4Style = {
-        textAlign: 'left',
-    }
-
-    const gameHistoryButton = {
+    const logoutButtonStyle = {
         background: 'none',
         border: 'none',
         cursor: 'pointer',
         width: 'fit-content',
-        minWidth: '80px',
-        height: '35px',
+        minWidth: '100px',
+        height: '50px',
         padding: '8px',
         borderRadius: '5px',
         boxShadow: '0px 0px 20px -20px',
-        backgroundColor: '#320c74',
+        backgroundColor: '#2b2725',
         userSelect: 'none',
         fontWeight: 'bold',
         color: 'white',
@@ -57,24 +45,28 @@ const Profile = () => {
     }, []);
 
     if (isLoading) {
-        return <div className="App">Loading...</div>;
+        return <Typography variant="h3" sx={bodyTypographyStyling}>Loading...</Typography>;
     }    
 
     return (
-        <>
-            <div style={overallDivStyle}>
-                {/* <img style={ProfilePicStyle} src={userProfilePic} alt="User profile"/> */}
-                <img style={ProfilePicStyle} src={userProfile} alt="User profile"/>
-                <h3>{username}</h3>
-                <h4 style={h4Style}>ELO: {userData.elo}</h4>
-                <h4 style={h4Style}> User ID: {userData.id}</h4>
-                <h4 style={h4Style}> Email: {useremail}</h4>
-                <h4 style={h4Style}> Longest Win Streak: 0 out of 0 games </h4>
-                <h4 style={h4Style}> Total wins: 0 games </h4>
-                <button style={gameHistoryButton}>View Game History</button>
-                <button style={gameHistoryButton} onClick={() => dispatch({type: "auth/logout"})}>Logout</button>
-            </div>
-        </>
+        <Box className="ProfileContainer">
+            <Typography variant="h3" sx={bodyTypographyStyling}>Profile</Typography>
+            <Box className="ProfileContent">
+                <Typography variant="h3" sx={bodyTypographyStyling}>{username}</Typography>
+                <br />
+                <Typography variant="h4" sx={bodyTypographyStyling}>ELO: {userData.elo}</Typography>
+                <br />
+                <Typography variant="h4" sx={bodyTypographyStyling}>User ID: {userData.id}</Typography>
+                <br />
+                <Typography variant="h4" sx={bodyTypographyStyling}>Email: {useremail}</Typography>
+                <br />
+                <Typography variant="h4" sx={bodyTypographyStyling}>Longest Win Streak: (not implemented) </Typography>
+                <br />
+                <Typography variant="h4" sx={bodyTypographyStyling}>Total wins: (also not implemented) </Typography>
+                <br />
+                <Button style={logoutButtonStyle} onClick={() => dispatch({type: "auth/logout"})}>Logout</Button>
+            </Box>
+        </Box>
     );
 };
 
